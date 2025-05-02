1839806cc62fa45a33b2a97723960e060728244b
Alexey Anokhin <alexey.anokhin88@gmail.com>
2025-05-02 16:41:33 +0300

    Create new class Update create metods

--- url-shortener/api/api_v1/short_urls/crud.py
+++ url-shortener/api/api_v1/short_urls/crud.py
@@ -1,12 +1,41 @@
-from schemas.short_url import ShortUrl
+from pydantic import BaseModel, AnyHttpUrl
 
-SHORT_URLS = [
-    ShortUrl(
-        target_url="https://example.com",
+from schemas.short_url import (
+    ShortUrl,
+    ShortUrlCreate,
+)
+
+
+# Создаем класс со словарем, в котором будем вытаскивать ссылку по slug-ключу
+class ShortUrlsStorage(BaseModel):
+    slug_to_short_url: dict[str, ShortUrl] = {}
+
+    # Метод получения slug ключей
+    def get(self) -> list[ShortUrl]:
+        return list(self.slug_to_short_url.values())
+
+    def get_by_slug(self, slug: str) -> ShortUrl | None:
+        return self.slug_to_short_url.get(slug)
+
+    def create(self, short_url_in: ShortUrlCreate) -> ShortUrl:
+        short_url = ShortUrl(
+            **short_url_in.model_dump(),
+        )
+        self.slug_to_short_url[short_url.slug] = short_url
+        return short_url
+
+
+storage = ShortUrlsStorage()
+
+storage.create(
+    ShortUrlCreate(
+        target_url=AnyHttpUrl("https://example.com"),
         slug="example",
     ),
-    ShortUrl(
-        target_url="https://google.com",
+)
+storage.create(
+    ShortUrlCreate(
+        target_url=AnyHttpUrl("https://google.com"),
         slug="search",
     ),
-]
+)
