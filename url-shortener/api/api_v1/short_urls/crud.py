aaa7bff2f690023114211bcb031db47e6f5780a3
Alexey Anokhin <alexey.anokhin88@gmail.com>
2025-05-17 18:01:18 +0300

    Create new def

--- url-shortener/api/api_v1/short_urls/crud.py
+++ url-shortener/api/api_v1/short_urls/crud.py
@@ -30,6 +30,22 @@ class ShortUrlsStorage(BaseModel):
             return ShortUrlsStorage()
         return cls.model_validate_json(SHORT_URLS_STORAGE_FILEPATH.read_text())
 
+    def init_storage_from_state(self) -> None:
+        try:
+            data = ShortUrlsStorage.from_state()
+        except ValidationError:
+            self.save_state()
+            log.warning("Rewritten storage file due to validation error.")
+            return
+
+        # обновление свойства напрямую
+        # если будут новые свойства,
+        # то их тоже надо обновить.
+        self.slug_to_short_url.update(
+            data.slug_to_short_url,
+        )
+        log.warning("Recovered data from storage file.")
+
     # Метод получения slug ключей
     def get(self) -> list[ShortUrl]:
         return list(self.slug_to_short_url.values())
@@ -73,10 +89,4 @@ class ShortUrlsStorage(BaseModel):
         return short_url
 
 
-try:
-    storage = ShortUrlsStorage.from_state()
-    log.warning("Recovered data from storage file.")
-except ValidationError:
-    storage = ShortUrlsStorage()
-    storage.save_state()
-    log.warning("Rewritten storage file due to validation error.")
+storage = ShortUrlsStorage()
