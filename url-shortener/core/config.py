8624619f0d90ad282df2fca7e333f97ac1d91a1d
Alexey Anokhin <alexey.anokhin88@gmail.com>
2025-05-27 09:14:11 +0300

    Add api_tokens

--- url-shortener/core/config.py
+++ url-shortener/core/config.py
@@ -10,3 +10,12 @@ LOG_LEVEL = logging.INFO
 LOG_FORMAT: str = (
     "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
 )
+
+# Never store real tokens here!
+# Only fake values
+API_TOKENS: frozenset[str] = frozenset(
+    {
+        "fMm9eOevFKZUuRZlA-Lagw",
+        "Gv70ciWBJErh_4Z2HB9l4g",
+    }
+)
