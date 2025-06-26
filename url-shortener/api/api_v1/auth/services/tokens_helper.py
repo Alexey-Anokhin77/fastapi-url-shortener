9596fb51baf0a71aea9f3a61e5c79ca4f802857d
Alexey Anokhin <alexey.anokhin88@gmail.com>
2025-06-26 16:29:44 +0300

    Create auth.services

---
+++ url-shortener/api/api_v1/auth/services/tokens_helper.py
@@ -0,0 +1,44 @@
+import secrets
+from abc import ABC, abstractmethod
+
+
+class AbstractTokensHelpers(ABC):
+    """
+    Что мне нужно от обертки:
+     - проверять на наличие токена
+     - добавлять токен в хранилище
+     - сгенерировать и добавить токен
+    """
+
+    @abstractmethod
+    def token_exists(
+        self,
+        token: str,
+    ) -> bool:
+        """
+        Check if token exist.
+        :param token:
+        :return:
+        """
+
+    @abstractmethod
+    def add_token(
+        self,
+        token: str,
+    ) -> None:
+        """
+        Save token in to storage.
+        :param token:
+        :return:
+        """
+
+    @classmethod
+    def generate_token(cls) -> str:
+        return secrets.token_urlsafe(16)
+
+    def generate_and_save_token(
+        self,
+    ) -> str:
+        token = self.generate_token()
+        self.add_token(token)
+        return token
---
+++ url-shortener/api/api_v1/auth/services/users_helper.py
@@ -0,0 +1,53 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractUsersHelpers(ABC):
+    """
+    Что мне нужно от обертки:
+     - получение пароля по юзернейму
+     - совпадает ли пароль по пользователю
+    """
+
+    @abstractmethod
+    def get_user_password(
+        self,
+        username: str,
+    ) -> str | None:
+        """
+        По переданному юзернейму находит пароль,
+        возвращает пароль, если есть.
+
+        :param username: - имя пользователя
+        :return: - возвращаем пароль по пользователю, если найден
+        """
+
+    @classmethod
+    def check_passwords_match(
+        cls,
+        password1: str,
+        password2: str,
+    ) -> bool:
+        """
+        Проверка паролей на совпадение.
+        """
+        return password1 == password2
+
+    def validate_user_password(
+        self,
+        username: str,
+        password: str,
+    ) -> bool:
+        """
+        Проверить, валиден ли пароль
+
+        :param username: - чей пароль проверить
+        :param password: - переданный пароль, сверить с тем, что в БД
+        :return: True если совпадает, иначе False
+        """
+        db_password = self.get_user_password(username)
+        if db_password is None:
+            return False
+        return self.check_passwords_match(
+            password1=db_password,
+            password2=password,
+        )
