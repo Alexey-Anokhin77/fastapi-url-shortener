d3617a3c96c9e25034da5fcd6be3b36b2aaf98a2
Alexey Anokhin <alexey.anokhin88@gmail.com>
2025-09-18 13:18:13 +0300

    fixture and refactoring

--- url-shortener/testing/conftest.py
+++ url-shortener/testing/conftest.py
@@ -1,7 +1,34 @@
+import random
+import string
+from collections.abc import Generator
 from os import getenv
 
 import pytest
 
+from api.api_v1.short_urls.crud import storage
+from schemas.short_url import ShortUrl, ShortUrlCreate
+
 if getenv("TESTING") != "1":
     msg = "Environment is not ready for testing"
     pytest.exit(msg)
+
+
+def create_short_url() -> ShortUrl:
+    short_url_in = ShortUrlCreate(
+        slug="".join(
+            random.choices(
+                string.ascii_letters,
+                k=8,
+            ),
+        ),
+        description="Some description",
+        target_url="https://www.example.com",
+    )
+    return storage.create(short_url_in)
+
+
+@pytest.fixture()
+def short_url() -> Generator[ShortUrl]:
+    short_url = create_short_url()
+    yield short_url
+    storage.delete(short_url)
